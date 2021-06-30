ec1a57c70175033bb38c0bab9713b436187576ff
= <abaj0005@student.monash.edu>
2021-06-30 19:24:46 +0530

    Added Layout For Detail Screen

--- androidFood2Fork/src/main/java/com/example/food2forkkmm/android/presentation/recipe_detail/RecipeDetailScreen.kt
+++ androidFood2Fork/src/main/java/com/example/food2forkkmm/android/presentation/recipe_detail/RecipeDetailScreen.kt
@@ -7,10 +7,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import com.example.food2forkkmm.android.presentation.components.RecipeImage
 import com.example.food2forkkmm.android.presentation.navigation.Screen
+import com.example.food2forkkmm.android.presentation.recipe_detail.components.RecipeView
 import com.example.food2forkkmm.android.presentation.recipe_list.components.RecipeCard
 import com.example.food2forkkmm.android.presentation.theme.AppTheme
 import com.example.food2forkkmm.domain.model.Recipe
 
+@ExperimentalStdlibApi
 @ExperimentalMaterialApi
 @ExperimentalComposeUiApi
 @Composable
@@ -22,9 +24,7 @@ fun RecipeDetailScreen(
             Text(text = "Unable to get details!!")
         }
         else{
-            RecipeCard(recipe = recipe,
-                onClick = {}
-            )
+            RecipeView(recipe = recipe)
         }
     }
 }
